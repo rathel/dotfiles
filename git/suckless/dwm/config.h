60a99238d78b39d507f1c30c7346cc9ef0821d69
rathel <rathel@gmail.com>
2020-04-03 18:58:00 -0600

    changed colors

--- git/suckless/dwm/config.h
+++ git/suckless/dwm/config.h
@@ -3,7 +3,7 @@
 #include <X11/XF86keysym.h>
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int borderpx  = 0;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
@@ -14,10 +14,12 @@ static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#005577";
+static const char col_darken[]      = "#1d1d1d";
+static const char col_darkpurple[]  = "#630cba";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeNorm] = { col_gray3, col_darken, col_gray2 },
+	[SchemeSel]  = { col_gray4, col_darkpruple,  col_cyan  },
 };
 
 /* tagging */
