b245834fd5b8d183013cf3111e94ac98b1a42910
rathel <rathel@gmail.com>
2020-04-05 10:53:54 -0600

    added headphone indicator

--- dot_scripts/executable_dwm_status.sh
+++ dot_scripts/executable_dwm_status.sh
@@ -9,11 +9,24 @@ get_date(){
 }
 
 get_volume(){
-	echo "Volume: $(pactl list sinks | grep '^[[:space:]]Volume:' | \
+mute="$(pactl list sinks | grep Mute | awk '{print $2}')"
+	if [ $mute = yes ]; then
+		echo "Muted"
+	else
+		echo "$(pactl list sinks | grep '^[[:space:]]Volume:' | \
 		head -n $(( $SINK + 1 )) | tail -n 1 | sed -e 's,.* \([0-9][0-9]*\)%.*,\1,')%"
+	fi
 	    }
 
+test_headphone(){
+	headphone_test="$(pactl list sinks | grep Active | awk '{print $3}')"
+	if [ $headphone_test = analog-output-headphones ]; then
+		echo "Volume_H:"
+	else
+		echo "Volume_S:"
+	fi
+	}
 while true; do
-	xsetroot -name "$(get_volume) | $(get_battery) | $(get_date)"
+	xsetroot -name "$(test_headphone)$(get_volume) | $(get_battery) | $(get_date)"
 	sleep 2
 done
