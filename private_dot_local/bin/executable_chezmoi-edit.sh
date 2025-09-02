e30771a37e2a90701f371e1751e2f324d576d952
TimH <rathel@gmail.com>
2025-09-01 18:24:37 -0600

    Update .local/bin/chezmoi-edit.sh

--- private_dot_local/bin/executable_chezmoi-edit.sh
+++ private_dot_local/bin/executable_chezmoi-edit.sh
@@ -1,7 +1,7 @@
 #!/bin/env bash
 set -euo pipefail
 
-selection=$(chezmoi managed -p absolute -x dirs | sk)
+selection=$(chezmoi managed -p absolute -x dirs | sk --with-nth1)
 
 [ -z "$selection" ] && exit 1
 
