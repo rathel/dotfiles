df024938428f65113bb00270698653c8456cff29
TimH <rathel@gmail.com>
2025-09-03 14:49:10 -0600

    Update .config/sk_options.sh

--- dot_config/sk_options.sh
+++ dot_config/sk_options.sh
@@ -2,6 +2,6 @@ sk_options=(
 	--ansi 
 	--with-nth=1 
 	--delimiter=$'\t'
-	--preview="bat --color=always {}"
-	--preview-window="right:50%:wrap"
+	# --preview="bat --color=always {}"
+	# --preview-window="right:50%:wrap"
 )
