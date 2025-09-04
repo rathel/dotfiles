052781f95cee7c129232117a58b269dc4db53aee
TimH <rathel@gmail.com>
2025-09-03 23:50:41 -0600

    Add .local/bin/launch-players.sh

---
+++ private_dot_local/bin/executable_launch-players.sh
@@ -0,0 +1,16 @@
+#!/bin/env bash
+
+set -euo pipefail
+
+sites=(
+	"Amazon Prime Video	https://amazon.com/gp/video/storefront"
+	"Plex	https://app.plex.tv/desktop"
+	"Netflix	https://netflix.com"
+	"Spotify	https://spotify.com"	
+)
+
+selection=$(printf "%s\n" "${sites[@]}" | sk --with-nth=1 --delimiter=$'\t')
+cmd="${selection#*$'\t'}"
+
+# firefox -P kiosk --kiosk "$cmd"
+microsoft-edge-stable --app="$cmd"
