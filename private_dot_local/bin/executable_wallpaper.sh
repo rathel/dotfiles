06388253f9aea473166562e4a6dd93c7e3251fe1
TimH <rathel@gmail.com>
2025-09-17 09:27:50 -0600

    Add .local/bin/wallpaper.sh

---
+++ private_dot_local/bin/executable_wallpaper.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+wallpapers="$HOME/Wallpapers"
+
+if command -v swww &> /dev/null; then
+	if ! pgrep -x "swww-daemon" > /dev/null; then
+		nohup swww-daemon >/dev/null 2>&1 &
+	fi
+else
+	echo "swww command not found. Please install swww to set the wallpaper."
+	exit 1
+fi
+
+while :; do
+	wallpaper="$(fd . $wallpapers -e jpg -e png | shuf -n1)"
+	notify-send -i "$wallpaper" "Setting Wallpaper to:" "$(basename $wallpaper)"
+	swww img "$wallpaper" >/dev/null 2>&1 &
+	sleep 1h
+done
