e1add33d4c40e525cfa227d42c1f7df89ecd564f
rathel <rathel@gmail.com>
2025-09-19 02:38:44 -0600

    Update .local/bin/launch-shadowtech.sh

--- private_dot_local/bin/executable_launch-shadowtech.sh
+++ private_dot_local/bin/executable_launch-shadowtech.sh
@@ -34,7 +34,7 @@ if [ -n "$browser" ]; then
 		notify-send "Launching ShadowTech with Iron..."
 		# setsid -f sh -c "vivaldi-stable --ozone-platform=wayland --app=https://pc.shadow.tech" &
 		nohup sh -c "$HOME/Applications/iron-linux-64/chrome $common_options" >/dev/null 2>&1 &
-		sleep 0.5
+		sleep 1
 		exit 0
 	elif [ "$browser" == Firefox ]; then
 		notify-send "Launching ShadowTech with Firefox..."
