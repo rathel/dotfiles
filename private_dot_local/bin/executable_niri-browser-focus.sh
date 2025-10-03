a038fc5e724c54b0dafcc860ae5c5714bf2c90dc
TimH <rathel@gmail.com>
2025-10-02 19:52:56 -0600

    Update .local/bin/niri-browser-focus.sh

--- private_dot_local/bin/executable_niri-browser-focus.sh
+++ private_dot_local/bin/executable_niri-browser-focus.sh
@@ -1,25 +1,28 @@
 #!/bin/bash
 
-browser="zen"
+browser="zen-twilight"
 win_id=$(niri msg -j windows | jq -r --arg app "$browser" '.[] | select(.app_id == $app) | .id')
 
 if [ -n "$win_id" ]; then
     # Focus that specific window
     niri msg action focus-window --id "$win_id"
 else
-	case "$browser" in
-		"zen")
-			$HOME/Applications/zen-x86_64.AppImage
-			;;
-		"firefox")
-			firefox
-			;;
-		"edge")
-			microsoft-edge-stable
-			;;
-		*)
-			echo "Unknown browser: $browser"
-			exit 1
-			;;
-	esac
+    case "$browser" in
+        "zen")
+        $HOME/Applications/zen-x86_64.AppImage
+        ;;
+        "zen-twilight")
+        $HOME/Applications/zen-twilight/zen
+        ;;
+        "firefox")
+        firefox
+        ;;
+        "edge")
+        microsoft-edge-stable
+        ;;
+        *)
+            echo "Unknown browser: $browser"
+            exit 1
+            ;;
+    esac
 fi
