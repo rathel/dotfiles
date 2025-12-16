06969aa2c9066dff31b8e8a0b4faa4ca644269a9
Tim <rathel@gmail.com>
2025-12-16 15:10:37 -0700

    Update .local/bin/niri-thunderbird-focus.sh

--- private_dot_local/bin/executable_niri-thunderbird-focus.sh
+++ private_dot_local/bin/executable_niri-thunderbird-focus.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Look for the window ID of a Firefox window
-win_id=$(niri msg -j windows | jq -r --arg app "thunderbird" '.[] | select(.app_id == $app) | .id')
+win_id=$(niri msg -j windows | jq -r --arg app "thunderbird-beta" '.[] | select(.app_id == $app) | .id')
 
 if [ -n "$win_id" ]; then
     # Focus that specific window
