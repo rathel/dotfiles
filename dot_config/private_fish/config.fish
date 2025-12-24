e13932b1649581ca50e85629eb893bf3776de167
Tim <rathel@gmail.com>
2025-12-24 16:53:32 -0700

    Update .config/fish/config.fish

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -2,7 +2,7 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
     set -g fish_key_bindings fish_vi_key_bindings
     set -gx EDITOR nvim
-    set -ax PATH $HOME/.local/bin $HOME/.npm-global/bin
+    set -gx PATH $HOME/.local/bin $HOME/.npm-global/bin
     set fish_greeting
     # wallust_ssh
     # tmux_ssh
