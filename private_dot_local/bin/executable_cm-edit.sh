b1b0d5942cac53bbe1aa40c1e35ffd05c3e10a10
Tim <rathel@gmail.com>
2026-01-22 16:28:29 -0700

    Edited: cm-edit.sh

--- private_dot_local/bin/executable_cm-edit.sh
+++ private_dot_local/bin/executable_cm-edit.sh
@@ -1,14 +1,16 @@
 #!/bin/env bash
 set -euo pipefail
 
-editor_dir="$HOME/Applications/Productivity"
-
-editor="${1:-nvim}"
-
 file="$(chezmoi managed --include=files --path-style=absolute --source-path | sk --preview="bat {}" --preview-window right:30%)"
 file="$(chezmoi source-path "$file")"
 
-if [ -n "$file" ]; then
-    # "$editor" "$file"
-    basename "$file"
+exec nvim "$file"
+
+if [[-n $(chezmoi git -- status --porcelain=v1) ]]; then
+	echo "No changes made to $file"
+	exit 0
+else
+	exec chezmoi git add "$file"
+	exec chezmoi git commit -m "Edited: $file"
+	exec chezmoi git push
 fi
