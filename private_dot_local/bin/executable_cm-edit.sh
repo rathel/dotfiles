3f329243796fc73b13f080d98341fc4f840c277e
rathel <rathel@opensuse-g750jh.tail836702.ts.net>
2026-01-27 07:34:53 -0700

    Updates

--- private_dot_local/bin/executable_cm-edit.sh
+++ private_dot_local/bin/executable_cm-edit.sh
@@ -6,11 +6,11 @@ file="$(chezmoi source-path "$file")"
 
 exec nvim "$file"
 
-if [[-n $(chezmoi git -- status --porcelain=v1) ]]; then
-	echo "No changes made to $file"
-	exit 0
+if [[-n "$(chezmoi git -- status --porcelain=v1)" ]]; then
+  echo "No changes made to $file"
+  exit 0
 else
-	exec chezmoi git add "$file"
-	exec chezmoi git commit -m "Edited: $file"
-	exec chezmoi git push
+  exec chezmoi git add "$file"
+  exec chezmoi git commit -m "Edited: $file"
+  exec chezmoi git push
 fi
