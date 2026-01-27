5c1c7ec6498b2d99a2cf684a540a9f8bc351f7c3
rathel <rathel@opensuse-g750jh.tail836702.ts.net>
2026-01-27 07:44:59 -0700

    Edited: /home/rathel/.local/share/chezmoi/private_dot_local/bin/executable_cm-edit.sh

--- private_dot_local/bin/executable_cm-edit.sh
+++ private_dot_local/bin/executable_cm-edit.sh
@@ -4,13 +4,13 @@ set -euo pipefail
 file="$(chezmoi managed --include=files --path-style=absolute --source-path | sk --preview="bat {}" --preview-window right:30%)"
 file="$(chezmoi source-path "$file")"
 
-exec nvim "$file"
+nvim "$file"
 
-if [[-z "$(chezmoi git -- status --porcelain=v1)" ]]; then
+if [[ -z "$(chezmoi git -- status --porcelain=v1)" ]]; then
   echo "No changes made to $file"
   exit 0
 else
-  exec chezmoi git -- add "$file"
-  exec chezmoi git -- commit -m "Edited: $file"
-  exec chezmoi git -- push
+  chezmoi git -- add "$file"
+  chezmoi git -- commit -m "Edited: $file"
+  chezmoi git -- push
 fi
