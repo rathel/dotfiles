00efbaaa69abceae58c0aa78479efe9c2d7d197e
rathel <rathel@gmail.com>
2026-03-02 19:51:30 -0700

    /home/rathel/.local/share/chezmoi/private_dot_local/bin/executable_niri-browser-focus.sh

--- private_dot_local/bin/executable_niri-browser-focus.sh
+++ private_dot_local/bin/executable_niri-browser-focus.sh
@@ -1,32 +1,32 @@
 #!/bin/bash
 
-browser="zen-twilight"
+browser="zen"
 win_id=$(niri msg -j windows | jq -r --arg app "$browser" '.[] | select(.app_id == $app) | .id')
 
 if [ -n "$win_id" ]; then
-    # Focus that specific window
-    niri msg action focus-window --id "$win_id"
+  # Focus that specific window
+  niri msg action focus-window --id "$win_id"
 else
-    case "$browser" in
-        "zen")
-		zen-browser
-        ;;
-        "zen-twilight")
-        $HOME/Applications/Utilities/zen-twilight/zen
-        ;;
-        "firefox")
-        firefox
-        ;;
-        "edge")
-        microsoft-edge-stable
-        ;;
-        "vivaldi-stable")
-        vivaldi
-        ;;
+  case "$browser" in
+  "zen")
+    zen-browser
+    ;;
+  "zen-twilight")
+    $HOME/Applications/Utilities/zen-twilight/zen
+    ;;
+  "firefox")
+    firefox
+    ;;
+  "edge")
+    microsoft-edge-stable
+    ;;
+  "vivaldi-stable")
+    vivaldi
+    ;;
 
-        *)
-            echo "Unknown browser: $browser"
-            exit 1
-            ;;
-    esac
+  *)
+    echo "Unknown browser: $browser"
+    exit 1
+    ;;
+  esac
 fi
