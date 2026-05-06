9c004beaf012ac6f6d7edd6e40754292ef9a5268
rathel <rathel@gmail.com>
2026-05-06 15:36:33 -0600

    /home/rathel/.local/share/chezmoi/dot_config/wezterm/wezterm.lua

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -1,9 +1,11 @@
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 -- Basic configuration
-config.font = wezterm.font 'Iosevka Nerd Font'
-config.color_scheme = 'Catppuccin Mocha'
+config.font = wezterm.font("Iosevka Nerd Font")
+config.color_scheme = "Catppuccin Mocha"
 config.window_decorations = "RESIZE"
+config.front_end = "OpenGL"
 -- Make the terminal background slightly transparent (0.0 = fully transparent, 1.0 = opaque)
 config.window_background_opacity = 0.9
 return config
+
