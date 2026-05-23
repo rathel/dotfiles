cf31e76bbc060a00ade78ef61d325b62663c0bfe
rathel <rathel@gmail.com>
2026-05-22 19:23:48 -0600

    launch-shadowtech.sh

--- private_dot_local/bin/executable_launch-shadowtech.sh
+++ private_dot_local/bin/executable_launch-shadowtech.sh
@@ -1,50 +1,51 @@
 #!/bin/env bash
 
 browsers=(
-	"Chrome"
-	"Edge"
-	"Firefox"
-	"Vivaldi"
+  "Chrome"
+  "Edge"
+  "Firefox"
+  "Vivaldi"
 )
 
 set +u
 source "$HOME/.myenv"
 set -u
 
-
 common_options="--ozone-platform=wayland --app=https://pc.shadow.tech"
 
-printf "%s\n" "${browsers[@]}" | sk --prompt "Select browser to launch ShadowTech: " --height 10 --ansi | while read -r browser; do
-if [ -n "$browser" ]; then
-	if [ "$browser" == Edge ]; then
-		notify-send "Launching ShadowTech with Microsoft Edge..."
-		setsid -w sh -c "microsoft-edge-stable $common_options" >/dev/null 2>&1 &
-		sleep 0.5
-		exit 0
-	elif [ "$browser" == Chrome ]; then
-		notify-send "Launching ShadowTech with Google Chrome..."
-		# setsid -f sh -c "google-chrome-stable --ozone-platform=wayland --app=https://pc.shadow.tech" &
-		setsid -w sh -c "chromium --app=https://pc.shadow.tech" >/dev/null 2>&1 &
-		sleep 0.5
-		exit 0
-	elif [ "$browser" == Vivaldi ]; then
-		notify-send "Launching ShadowTech with Vivaldi..."
-		setsid -w sh -c "vivaldi-stable $common_options" >/dev/null 2>&1 &
-		sleep 0.5
-		exit 0
-	elif [ "$browser" == Firefox ]; then
-		notify-send "Launching ShadowTech with Firefox..."
-		setsid -w sh -c "firefox --new-window https://pc.shadow.tech" >/dev/null 2>&1 &
-		sleep 0.5
-		exit 0
+browser=$(
+  printf "%s\n" "${browsers[@]}" |
+    fuzzel --dmenu --prompt "Select browser to launch ShadowTech: " --lines "${#browsers[@]}"
+)
 
-	else
-		echo "Usage: $0 {chrome|edge|vivaldi}"
-		exit 1
-	fi
+case "$browser" in
+Edge)
+  notify-send "Launching ShadowTech with Microsoft Edge..."
+  setsid -w sh -c "microsoft-edge-stable $common_options" >/dev/null 2>&1 &
+  ;;
+Chrome)
+  notify-send "Launching ShadowTech with Google Chrome..."
+  # setsid -f sh -c "google-chrome-stable --ozone-platform=wayland --app=https://pc.shadow.tech" &
+  setsid -w sh -c "chromium --app=https://pc.shadow.tech" >/dev/null 2>&1 &
+  ;;
+Vivaldi)
+  notify-send "Launching ShadowTech with Vivaldi..."
+  setsid -w sh -c "vivaldi-stable $common_options" >/dev/null 2>&1 &
+  ;;
+Firefox)
+  notify-send "Launching ShadowTech with Firefox..."
+  setsid -w sh -c "firefox --new-window https://pc.shadow.tech" >/dev/null 2>&1 &
+  ;;
+"")
+  exit 0
+  ;;
+*)
+  echo "Usage: $0 {chrome|edge|firefox|vivaldi}"
+  exit 1
+  ;;
+esac
 
-fi
-done
+sleep 0.5
 
 # gamescope -f -w 1920 -h 1080 -W 1920 -H 1080 --force-grab-cursor --backend sdl -- google-chrome-stable --app=https://pc.shadow.tech
 #
